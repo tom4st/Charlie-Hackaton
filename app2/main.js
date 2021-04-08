1ddd26b2840005427ec9848c83ee343ce72306e3
tt <tom4st@gmail.com>
2021-04-08 22:49:31 +0100

    skeleton html v2

---
+++ app2/main.js
@@ -0,0 +1,3 @@
+var myFormData = [];   //declare an array
+var value1 = document.getElementById("myTextInput").value;  //get the value of an element by it's id
+myFormData.push(value1); //put to the array
